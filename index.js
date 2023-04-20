4fd30bcc6406da703a1364afdea6817f17beebca
Eric Broñola <eric.bronola@mgenesis.local>
2023-04-20 13:11:41 +0800

    test3

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worldssxx!');
-});
+app.set('view engine', 'ejs')
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.use(express.static('public'))
+
+app.get('/',(req, res) => {
+	res.render('index')
+})
+
+server = app.listen(process.env.PORT || 5000)
+let connections = [];
+const io = require("socket.io")(server,{
+	cors: {
+		origin: '*',
+	}
+	})
+io.on('connection',  (socket) => {
+	let userConnected = socket.handshake.query.user;
+	let userIdConnected = socket.handshake.query.userId;
+    connections.push(socket.handshake);
+	console.log(`user connected : ${userConnected} `);
+	socket.on(`req`, (callback)=>{
+		console.log('sharepoint')
+		callback = typeof callback == "function" ? callback : () => {};
+		socket.broadcast.emit(`request_data`, {user:userConnected, userId:userIdConnected})
+	})
+	socket.on('send_data', (data)=>{
+		console.log(data)
+		socket.broadcast.emit(`send`, {data:data})
+	})
+
+	socket.on(`dashboard_req`, (callback)=>{
+		console.log('sharepoint')
+		callback = typeof callback == "function" ? callback : () => {};
+		socket.broadcast.emit(`dashboard_data`, {user:userConnected, userId:userIdConnected})
+	})
+
+	socket.on('send_data_to_dashboard', (data)=>{
+		console.log(data)
+		socket.broadcast.emit(`send_to_dashboard`, {data:data})
+	})
+	
+})
