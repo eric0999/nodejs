73a26bd4ffa1e257f6b13151c152679db9eefd90
Eric Broñola <eric.bronola@mgenesis.local>
2023-06-29 09:36:31 +0800

    updated server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+let tempData = [];
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
@@ -8,6 +8,10 @@ app.use(express.static('public'))
 app.get('/',(req, res) => {
 	res.render('index')
 })
+
+app.get("/api/inv", (req, res) => {
+	res.send(tempData);
+  });
 
 server = app.listen(process.env.PORT || 5000)
 let connections = [];
@@ -21,24 +25,63 @@ io.on('connection',  (socket) => {
 	let userIdConnected = socket.handshake.query.userId;
     connections.push(socket.handshake);
 	console.log(`user connected : ${userConnected} `);
-	socket.on(`req`, (callback)=>{
-		console.log('sharepoint')
-		callback = typeof callback == "function" ? callback : () => {};
-		socket.broadcast.emit(`request_data`, {user:userConnected, userId:userIdConnected})
+	// socket.on(`req`, (callback)=>{
+	// 	callback = typeof callback == "function" ? callback : () => {};
+	// 	socket.broadcast.emit(`request_data`, {user:userConnected, userId:userIdConnected})
+	// })
+	socket.on('updateLineItem',(data)=>{
+		socket.broadcast.emit(`postLineItem`,{
+			Amount:data.Amount,
+            CostCredit:data.CostCredit,
+            DocNum:data.DocNum_,
+            ID:data.ID,
+            Id:data.Id,
+		})
 	})
-	socket.on('send_data', (data)=>{
-		console.log(data)
-		socket.broadcast.emit(`send`, {data:data})
+	socket.on('updateLineItemData',(data)=>{
+		socket.broadcast.emit(`postLineItemData`,{
+			Amount:data.Amount,
+            CostCredit:data.CostCredit,
+            DocNum:data.DocNum_,
+            ID:data.ID,
+            Id:data.Id,
+		})
 	})
 
-	socket.on(`dashboard_req`, (callback)=>{
+	socket.on(`req`, (msg, callback)=>{
+		socket.broadcast.emit(`user_enter`, {user:userConnected, userId:userIdConnected})
+		console.log('sharepoint',msg);
+		if(tempData.length > 0){
+			callback(tempData);
+		}else{
+			socket.broadcast.emit(`request_data`,{user:userConnected, userId:userIdConnected});
+		}
+		})
+	
+	socket.on('connectToCloud', (data)=>{
+		tempData = []
+	})
+	socket.on('checkConnection', (msg, callback)=>{
+		console.log(msg)
+		callback('connected to cloud')
+	})
+	socket.on('send_data', (data)=>{
+		tempData.push(data)
+	})
+
+	socket.on('openItem', (data)=>{
+		socket.broadcast.emit(`userOpenItem`, {user:data.user, DocNum:data.docNum})
+	})
+
+
+	socket.on(`dashboard_req`, (data)=>{
 		console.log('sharepoint')
-		callback = typeof callback == "function" ? callback : () => {};
-		socket.broadcast.emit(`dashboard_data`, {user:userConnected, userId:userIdConnected})
+		console.log(data.daterange)
+		socket.broadcast.emit(`dashboard_data`, {user:userConnected, userId:userIdConnected, daterange:data.daterange})
 	})
 
 	socket.on('send_data_to_dashboard', (data)=>{
-		console.log(data)
+		//console.log(data)
 		socket.broadcast.emit(`send_to_dashboard`, {data:data})
 	})
 	
